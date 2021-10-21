479af720fc25ac1ab689c15f7c563eda7d54f7c1
Frank Martin <Martin.Frank@finnova.com>
2021-10-21 10:18:27 +0200

    added database sqls...

---
+++ src/main/sql/create_employees.sql
@@ -0,0 +1,3 @@
+USE crmflow;
+CREATE TABLE IF NOT EXISTS employees (firstname VARCHAR(1000), lastname VARCHAR(1000), title VARCHAR(1000), email VARCHAR(1000), notes VARCHAR(1000) );
+
---
+++ src/main/sql/create_employes_data.sql
@@ -0,0 +1,5 @@
+USE crmflow;
+
+INSERT INTO employees VALUES ('Dev', 'Eloper', 'developer', 'dev.eloper@dontsendthis.org', 'Backend developer');
+INSERT INTO employees VALUES ('Front', 'Ender', 'developer', 'front.ender@dontsendthis.org', 'Frontend developer');
+INSERT INTO employees VALUES ('Wo', 'Manager', 'manager', 'wo.manager@dontsendthis.org', 'PO and team leader');
